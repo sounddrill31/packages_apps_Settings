6fbef39b145ddf1625a804aaefc0e8ced0755f17
aswin7469 <aswinas@pixysos.com>
2023-01-14 00:52:35 +0530

    Settings: Add SkylineUI Workstation
    * inspired from project kaleidoscope
    * Thanks PixysOS
    
    Change-Id: I330566091da77857e34753c7a616259135b6561b
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -22,6 +22,7 @@ import com.android.settings.MainClear;
 import com.android.settings.MainClearConfirm;
 import com.android.settings.ResetNetwork;
 import com.android.settings.Settings;
+import com.skylineui.settings.laboratory.LabSettings;
 import com.android.settings.TestingSettings;
 import com.android.settings.TrustedCredentialsSettings;
 import com.android.settings.accessibility.AccessibilityDetailsSettingsFragment;
@@ -394,7 +395,8 @@ public class SettingsGateway {
             ResetNetwork.class.getName(),
             VibrationIntensitySettingsFragment.class.getName(),
             LiveDisplaySettings.class.getName(),
-            WifiTetherClientManager.class.getName()
+            WifiTetherClientManager.class.getName(),
+            LabSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
